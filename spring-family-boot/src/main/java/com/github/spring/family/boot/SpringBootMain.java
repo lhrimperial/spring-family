8228930f3f74e175a9ccd992f233d601b4096630
hairen.lhr <hairen.lhr@alibaba-inc.com>
2021-08-05 19:55:58 +0800

    first commit

---
+++ spring-family-boot/src/main/java/com/github/spring/family/boot/SpringBootMain.java
@@ -0,0 +1,16 @@
+package com.github.spring.family.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hairen.long
+ * @date 2021/1/24
+ */
+@SpringBootApplication
+public class SpringBootMain {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMain.class, args);
+  }
+}
