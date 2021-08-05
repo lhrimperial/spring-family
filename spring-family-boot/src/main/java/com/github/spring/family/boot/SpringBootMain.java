deeeb65ebf39ad80e45b489d900a30697de7c5bd
chuzhang <chuzhang.zc@alibaba-inc.com>
2021-08-05 21:05:35 +0800

    ttt

--- spring-family-boot/src/main/java/com/github/spring/family/boot/SpringBootMain.java
+++ spring-family-boot/src/main/java/com/github/spring/family/boot/SpringBootMain.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMain {
 
   public static void main(String[] args) {
+    //
     SpringApplication.run(SpringBootMain.class, args);
   }
 }
